b50d2bcce498d6c91c913b2f3021121790760629
Golful <109176543+Golful@users.noreply.github.com>
2024-05-06 10:56:48 +0000

    edit credentials 03

--- 03-building-a-data-warehouse/etl_bigquery.py
+++ 03-building-a-data-warehouse/etl_bigquery.py
@@ -36,7 +36,7 @@ def main(dataset_id, table_id, file_path):
     # keyfile = os.environ.get("KEYFILE_PATH")
     #
     # แต่เพื่อความง่ายเราสามารถกำหนด File Path ไปได้เลยตรง ๆ
-    keyfile = "insert_keyfile_path_here"
+    keyfile = "../credentials/ds525-load-data-to-bigquery.json"
     service_account_info = json.load(open(keyfile))
     credentials = service_account.Credentials.from_service_account_info(service_account_info)
 
